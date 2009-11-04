52649b0c269f75cc473b9ad0e1ef06debb5e39bb
Luke Kanies <luke@madstop.com>
2009-11-04 17:23:38 -0500

    Correcting comments and making report timestamp internal
    We had some no-longer-correct comments in the Transaction
    class, which are now removed.  This also moves the timestamp
    for reports into the report class, so it's created at
    initialization by the report, rather than by the transaction.
    
    Signed-off-by: Luke Kanies <luke@madstop.com>

--- lib/puppet/transaction.rb
+++ lib/puppet/transaction.rb
@@ -56,8 +56,7 @@ class Puppet::Transaction
         end
     end
 
-    # Apply all changes for a resource, returning a list of the events
-    # generated.
+    # Apply all changes for a resource
     def apply(resource)
         begin
             changes = resource.evaluate
@@ -189,11 +188,6 @@ class Puppet::Transaction
             end
         end
 
-        # A bit of hackery here -- if skipcheck is true, then we're the
-        # top-level resource.  If that's the case, then make sure all of
-        # the changes list this resource as a proxy.  This is really only
-        # necessary for rollback, since we know the generating resource
-        # during forward changes.
         unless children.empty?
             @changes[changecount..-1].each { |change| change.proxy = resource }
         end
@@ -331,8 +325,6 @@ class Puppet::Transaction
         @report.newmetric(:changes,
             :total => @changes.length
         )
-
-        @report.time = Time.now
 
         return @report
     end
